b56e7227f31776e75a38856666c2d1da0e8cce32
devansh <devanshshah7560@gmail.com>
2021-03-08 01:53:40 +0530

    vgg9 training

--- cifar/hyperparameters/vgg9_cifar10_baseline.py
+++ cifar/hyperparameters/vgg9_cifar10_baseline.py
@@ -4,10 +4,10 @@ config = dict(
     optimizer='SGD',
     optimizer_decay_at_epochs=[30, 60, 90, 120, 150, 180, 210, 240, 270],
     optimizer_decay_with_factor=2.0,
-    optimizer_learning_rate=0.05,
+    optimizer_learning_rate=0.01,
     optimizer_momentum=0.9,
     optimizer_weight_decay=0.0005,
-    batch_size=128,
+    batch_size=32,
     num_epochs=300,
     seed=42,
 )
